0c3684f54fcfb5f8b554cd3927fd382cf3cee14f
Ha young, Kim <jeremiah91@naver.com>
2021-11-04 15:22:58 +0900

    feat : Create Text Input

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { theme } from "./colors";
 
 export default function App() {
-  const [selected, setSelected] = useState(true);
-  const travel = () => setSelected(false);
-  const work = () => setSelected(true);
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
         <TouchableOpacity onPress={work}>
           <Text
-            style={{ ...styles.btnText, color: selected ? "#eee" : theme.gray }}
+            style={{
+              ...styles.btnText,
+              color: working ? theme.white : theme.gray,
+            }}
           >
             Work
           </Text>
@@ -22,13 +35,24 @@ export default function App() {
           <Text
             style={{
               ...styles.btnText,
-              color: !selected ? "#eee" : theme.gray,
+              color: !working ? theme.white : theme.gray,
             }}
           >
             Travel
           </Text>
         </TouchableOpacity>
       </View>
+      <TextInput
+        // returnKeyType="done"   : 엔터키 이름 변경 (returnKeyLabel : Android)
+        // secureTextEntry        : 비밀번호 input
+        // multiline              : 여러줄 입력
+        keyboardType="default"
+        style={styles.input}
+        placeholder={working ? "Add a To Do" : "Where do you want to go?"}
+        placeholderTextColor="#555"
+        onChangeText={onChangeText}
+        value={text}
+      />
     </View>
   );
 }
@@ -47,5 +71,16 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 40,
     fontWeight: "600",
+    marginRight: 12,
+    borderRightWidth: 1,
+    borderRightColor: "white",
+  },
+  input: {
+    backgroundColor: "#151515",
+    color: "#ccc",
+    padding: 16,
+    borderRadius: 8,
+    fontSize: 20,
+    marginTop: 20,
   },
 });
--- colors.js
+++ colors.js
@@ -1,4 +1,5 @@
 export const theme = {
   background: "black",
   gray: "#3a3d40",
+  white: "#eee",
 };
